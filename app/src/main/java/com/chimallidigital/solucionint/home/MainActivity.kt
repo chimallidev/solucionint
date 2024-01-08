cd78053d1e5c82406727b622fa0607092a253ec2
chimallidev <chimalli.dev@gmail.com>
2024-01-08 16:03:45 -0600

    initial commit

---
+++ app/src/main/java/com/chimallidigital/solucionint/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chimallidigital.solucionint.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chimallidigital.solucionint.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
