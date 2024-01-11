b36769b39f2fa4ec71af5ac608eaec7f269d715e
chimallidev <chimalli.dev@gmail.com>
2024-01-11 13:11:05 -0600

    scientific articles fragment xml configurado

---
+++ app/src/main/java/com/chimallidigital/solucionint/ui/scientific_articles/adapter/ScientificArticlesViewHolder.kt
@@ -0,0 +1,7 @@
+package com.chimallidigital.solucionint.ui.scientific_articles.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ScientificArticlesViewHolder(view: View): RecyclerView.ViewHolder(view) {
+}
