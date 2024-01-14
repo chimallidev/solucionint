33e450ffa341b36cd213fe19436590202f22300a
chimallidev <chimalli.dev@gmail.com>
2024-01-14 16:18:49 -0600

    enum class ScientificArticlesCategoriesModel creado

--- app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
@@ -2,6 +2,7 @@ package com.chimallidigital.solucionint.ui.selector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.chimallidigital.solucionint.R
 import com.chimallidigital.solucionint.databinding.ActivityArticlesSelectorBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,6 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ArticlesSelectorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityArticlesSelectorBinding
+    private val scientificArticlesSelectorViewModel: ArticlesSelectorViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityArticlesSelectorBinding.inflate(layoutInflater)
