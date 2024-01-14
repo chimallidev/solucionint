e9be8f26a0b40d81fa5f19cb6768075b6491d268
chimallidev <chimalli.dev@gmail.com>
2024-01-14 16:04:32 -0600

    selector package agregado

---
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
@@ -0,0 +1,17 @@
+package com.chimallidigital.solucionint.ui.selector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chimallidigital.solucionint.R
+import com.chimallidigital.solucionint.databinding.ActivityArticlesSelectorBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ArticlesSelectorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityArticlesSelectorBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityArticlesSelectorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
