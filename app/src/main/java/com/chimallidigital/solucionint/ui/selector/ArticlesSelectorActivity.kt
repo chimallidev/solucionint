9a74b8134d45195c2893dcecdc86d7af6534e705
chimallidev <chimalli.dev@gmail.com>
2024-01-15 23:03:02 -0600

    xml activity articles selector terminado

--- app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
@@ -3,6 +3,7 @@ package com.chimallidigital.solucionint.ui.selector
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.navArgs
 import com.chimallidigital.solucionint.R
 import com.chimallidigital.solucionint.databinding.ActivityArticlesSelectorBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,9 +12,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class ArticlesSelectorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityArticlesSelectorBinding
     private val scientificArticlesSelectorViewModel: ArticlesSelectorViewModel by viewModels()
+
+    private val args: ArticlesSelectorActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityArticlesSelectorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
     }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.tvTitularCategoriaSelector.text= getString(scientificArticlesSelectorViewModel.getCategoryName(args.type))
+    }
+
 }
--- app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorViewModel.kt
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorViewModel.kt
@@ -1,9 +1,25 @@
 package com.chimallidigital.solucionint.ui.selector
 
 import androidx.lifecycle.ViewModel
+import com.chimallidigital.solucionint.R
+import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategories
+import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategoriesModel
+import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategoriesModel.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ArticlesSelectorViewModel @Inject constructor(): ViewModel(){
+class ArticlesSelectorViewModel @Inject constructor(): ViewModel() {
+    lateinit var name: ScientificArticlesCategoriesModel
+
+    fun getCategoryName(category: ScientificArticlesCategoriesModel): Int {
+        name = category
+
+        val categoryName = when (name) {
+            Soluciones_Inteligentes -> R.string.soluciones_inteligentes
+            Ponte_en_Forma -> R.string.ponte_en_forma
+            Recetas_de_Cocina -> R.string.recetas_de_cocina
+        }
+        return categoryName
+    }
 }
