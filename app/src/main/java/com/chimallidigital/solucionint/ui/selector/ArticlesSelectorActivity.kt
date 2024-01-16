77c6b16859e0dacbcac52e1748b62b29a4695101
chimallidev <chimalli.dev@gmail.com>
2024-01-16 00:47:33 -0600

    boton back from Articles Selector Activity to Scientific Articles Fragment

--- app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
@@ -1,8 +1,13 @@
 package com.chimallidigital.solucionint.ui.selector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.navigation.NavController
 import androidx.navigation.navArgs
 import com.chimallidigital.solucionint.R
 import com.chimallidigital.solucionint.databinding.ActivityArticlesSelectorBinding
@@ -11,12 +16,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ArticlesSelectorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityArticlesSelectorBinding
+    private lateinit var navController: NavController
     private val scientificArticlesSelectorViewModel: ArticlesSelectorViewModel by viewModels()
 
     private val args: ArticlesSelectorActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityArticlesSelectorBinding.inflate(layoutInflater)
+        binding = ActivityArticlesSelectorBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
     }
@@ -26,7 +32,24 @@ class ArticlesSelectorActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        binding.tvTitularCategoriaSelector.text= getString(scientificArticlesSelectorViewModel.getCategoryName(args.type))
+        binding.tvTitularCategoriaSelector.text =
+            getString(scientificArticlesSelectorViewModel.getCategoryName(args.type))
+        binding.btnBackFromArticlesSelectorToArticlesFragment.setOnClickListener { backAnimation() }
     }
 
+    private fun backAnimation() {
+        val view = binding.activityArticlesSelector
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+        appearAnimation.duration = 1000
+        val viewBackAnimation = AnimatorInflater.loadAnimator(
+            view.context,
+            R.animator.back_zoom_in
+        ) as AnimatorSet
+
+        viewBackAnimation.apply {
+            setTarget(view)
+            doOnEnd { onBackPressedDispatcher.onBackPressed() }
+            start()
+        }
+    }
 }
