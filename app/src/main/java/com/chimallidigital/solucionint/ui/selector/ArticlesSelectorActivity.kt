b53f4c19c876f464ccfd3526ac3fef157f3b72af
chimallidev <chimalli.dev@gmail.com>
2024-01-18 15:49:06 -0600

    botones left and right recorren lista de categoria soluciones inteligentes

--- app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorActivity.kt
@@ -7,15 +7,24 @@ import android.view.animation.AlphaAnimation
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.chimallidigital.solucionint.R
 import com.chimallidigital.solucionint.databinding.ActivityArticlesSelectorBinding
+import com.chimallidigital.solucionint.domain.model.ArticlesSelector.CollectionArticles
+import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategoriesModel
+import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategoriesModel.*
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kotlin.math.max
 
 @AndroidEntryPoint
 class ArticlesSelectorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityArticlesSelectorBinding
     private val scientificArticlesSelectorViewModel: ArticlesSelectorViewModel by viewModels()
+    private var itemCount: Int = 0
 
     private val args: ArticlesSelectorActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +35,94 @@ class ArticlesSelectorActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
+        initUIState()
         initListeners()
+
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                scientificArticlesSelectorViewModel.articles.collect{
+                    initImageActiveSelector(it)
+                }
+            }
+        }
+    }
+
+    private fun initImageActiveSelector(articles: List<CollectionArticles>) {
+        val maxItems= articles.size
+        binding.tvSelectorMaximNumber.text= countFormatted(maxItems)
+        when(args.type){
+            Soluciones_Inteligentes -> {
+                binding.ivArticlesSelector.setImageResource(articles.get(itemCount).img)
+                binding.tvArticlesSelectorArticlesTitle.text= getString(articles.get(itemCount).title)
+            }
+            Ponte_en_Forma -> {}
+            Recetas_de_Cocina -> {}
+        }
     }
 
     private fun initListeners() {
+
         binding.tvTitularCategoriaSelector.text =
             getString(scientificArticlesSelectorViewModel.getCategoryName(args.type))
         binding.btnBackFromArticlesSelectorToArticlesFragment.setOnClickListener { backAnimation() }
+        binding.tvArticlesSelectorContador.text= countFormatted(itemCount+1)
+        binding.btnLeftArticlesSelector.setOnClickListener { decreaseCount(itemCount) }
+        binding.btnRightArticlesSelector.setOnClickListener { increaseCount(itemCount) }
+
+    }
+    private fun increaseCount(increaseCount: Int){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                scientificArticlesSelectorViewModel.articles.collect{
+                    val maxItemCount= it.size
+                    var newItemCount: Int
+                    newItemCount= increaseCount
+                    newItemCount = if(increaseCount< maxItemCount){
+                        increaseCount+1
+                    }else{ 1 }
+                    runOnUiThread {
+                        itemCount= newItemCount
+                        binding.ivArticlesSelector.setImageResource(it.get(itemCount-1).img)
+                        binding.tvArticlesSelectorArticlesTitle.text= getString(it.get(itemCount-1).title)
+                        binding.tvArticlesSelectorContador.text= countFormatted(itemCount)
+                        binding.tvSelectorMaximNumber.text= countFormatted(maxItemCount)
+                    }
+
+                }
+            }
+        }
+    }
+    private fun countFormatted(count: Int): CharSequence? {
+        val itemCountFormatted= String.format("%03d", count)
+        return itemCountFormatted
+    }
+
+    private fun decreaseCount(contadorDecrease: Int) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                scientificArticlesSelectorViewModel.articles.collect{
+                    val maxItemCount= it.size
+                    var newItemCount: Int
+                    newItemCount= contadorDecrease
+                    newItemCount = if(contadorDecrease> 1){
+                        contadorDecrease-1
+                    }else{ maxItemCount }
+                    runOnUiThread {
+                        itemCount= newItemCount
+
+                        binding.ivArticlesSelector.setImageResource(it.get(itemCount-1).img)
+                        binding.tvArticlesSelectorArticlesTitle.text= getString(it.get(itemCount-1).title)
+                        binding.tvArticlesSelectorContador.text= countFormatted(itemCount)
+                        binding.tvSelectorMaximNumber.text= countFormatted(maxItemCount)
+                    }
+
+                }
+            }
+        }
+
     }
 
     private fun backAnimation() {
--- app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorViewModel.kt
+++ app/src/main/java/com/chimallidigital/solucionint/ui/selector/ArticlesSelectorViewModel.kt
@@ -2,15 +2,26 @@ package com.chimallidigital.solucionint.ui.selector
 
 import androidx.lifecycle.ViewModel
 import com.chimallidigital.solucionint.R
+import com.chimallidigital.solucionint.data.providers.CollectionScientificArticlesProvider
+import com.chimallidigital.solucionint.domain.model.ArticlesSelector.CollectionArticles
 import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategories
 import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategoriesModel
 import com.chimallidigital.solucionint.domain.model.scientific_articles.ScientificArticlesCategoriesModel.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class ArticlesSelectorViewModel @Inject constructor(): ViewModel() {
+class ArticlesSelectorViewModel @Inject constructor(collectionScientificArticlesProvider: CollectionScientificArticlesProvider): ViewModel() {
+    private var _articles= MutableStateFlow<List<CollectionArticles>>(emptyList())
+    val articles: StateFlow<List<CollectionArticles>> = _articles
+
     lateinit var name: ScientificArticlesCategoriesModel
+
+    init {
+        _articles.value= collectionScientificArticlesProvider.getArticles()
+    }
 
     fun getCategoryName(category: ScientificArticlesCategoriesModel): Int {
         name = category
